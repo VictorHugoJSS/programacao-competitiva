f038afdf5904d5cd93376ae00a896193e56d51c0
Victor Hugo José Sales da Silva <120414185+VictorHugoJSS@users.noreply.github.com>
2024-01-17 14:38:32 -0300

    Update selo.cpp

--- Questions/selo.cpp
+++ Questions/selo.cpp
@@ -4,19 +4,32 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-typedef long long int llu;
+
+int prime(long long number){
+	long long divisions = 0;
+
+	if (number == 1 || number == 0) { return 0; }
+
+	for (long long int i = 2; i <= sqrt(number) ; i++){
+		if (number % i == 0){ return 1; }
+	}
+
+	return 0;
+}
 
 int main(){
-	llu selos;
+	long long selos;
+
+	ios::sync_with_stdio(0);
+	cin.tie(0);
 
 	cin >> selos;
 
-	if ((selos % 2 == 0) && (selos / 2 != 1)){
+	if (prime(selos)){
 		cout << "S\n";
 	}
 	else{
 		cout << "N\n";
 	}
-
 	return 0;
 }
