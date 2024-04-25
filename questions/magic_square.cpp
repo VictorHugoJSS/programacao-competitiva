71124d651af642613993e5a86c7c024526c11a86
Victor Hugo José Sales da Silva <victor.-hugo@outlook.com.br>
2024-04-25 08:22:51 -0300

    Update magic_square.cpp

--- questions/magic_square.cpp
+++ questions/magic_square.cpp
@@ -2,16 +2,16 @@
 
 using namespace std;
 
-int magic_square(int * arr, int size){
-    int sum_total = 0, sum = 0;
+int magic_square(int ** arr, int size){
+    int sum_total = 0, sum = 0, sumdg2 = 0;
     for (int i = 0; i < size; i++){
-        sum_total += *(((arr+0*size)+i));
+        sum_total += arr[0][i];
     }
 
     for (int i = 1; i < size; i++){
         sum = 0;
         for (int j = 0; j < size; j++){
-            sum += *(((arr+i*size)+j));
+            sum += arr[i][j];
         }
 
         if (sum != sum_total){
@@ -22,14 +22,22 @@ int magic_square(int * arr, int size){
     for (int i = 0; i < size; i++){
         sum = 0;
         for (int j = 0; j < size; j++){
-            sum += *(((arr+j*size)+i));
+            sum += arr[j][i];
         }
 
         if (sum != sum_total){
             return -1;
         }
     }
-
+    sum = 0;
+    for (int i = 0; i < size; i++){
+        sum += arr[i][i];
+        sumdg2 += arr[i][size-1-i];
+    }
+    
+    if (sum != sum_total || sumdg2 != sum_total){
+        return -1;
+    }
     return sum_total;
 }
 int main(){
@@ -38,14 +46,23 @@ int main(){
 
     int n;
     cin >> n;
-    int arr[n][n];
-
+    int **arr = (int **) malloc(n * sizeof(int **));
+    
+    for (int i = 0 ; i < n; i++){
+        arr[i] = (int *) malloc(n * sizeof(int *));
+    }
+    
     for (int i = 0; i < n; i++){
         for (int j = 0; j < n; j++){
             cin >> arr[i][j];
         }
     }
 
-    cout << magic_square((int *)arr, n) << "\n";
+    cout << magic_square(arr, n) << "\n";
+    
+    for (int i = 0 ; i < n; i++){
+        free(arr[i]);
+    }
+    free(arr);
     return 0;
 }
