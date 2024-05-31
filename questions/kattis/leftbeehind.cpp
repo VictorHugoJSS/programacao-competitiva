a25d82707aef58579c987339e7626df1395b7e47
VictorHugoJSS <victor.-hugo@outlook.com.br>
2024-05-31 10:02:38 -0300

    .

--- questions/kattis/leftbeehind.cpp
+++ questions/kattis/leftbeehind.cpp
@@ -4,10 +4,23 @@ using namespace std;
 
 int main(){
     int sweet, sour;
-
     cin >> sweet >> sour;
 
-    while (sweet && sour){
-        
+    while (sweet != 0 && sour != 0){
+
+        if (sour + sweet == 13){
+            printf("Never speak again.\n");
+        }
+        else if (sweet > sour){
+            printf("To the convention.\n");
+        }
+        else if (sweet < sour){
+            printf("Left beehind.\n");
+        }
+        else{
+            printf("Undecided.\n");
+        }
+
+        cin >> sweet >> sour;
     }
 }
