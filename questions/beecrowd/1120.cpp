ad8a6cb4de4d91bd0bc3beb15907d836583b90f1
VictorHugoJSS <victor.-hugo@outlook.com.br>
2024-06-28 16:20:55 -0300

    .

---
+++ questions/beecrowd/1120.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string String_Correction(string number, char digit){
+
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    char digit;
+    string number;
+
+    cin >> digit >> number;
+
+    while ( digit != '0' || number.compare("0") != 0 ){
+        
+        cin >> digit >> number;
+    }
+}
--- questions/uva/11078.cpp
+++ questions/uva/11078.cpp
@@ -1,20 +1,45 @@
-#include <bits/stdc++.h>
-
+#include <iostream>
+#include <climits>
 using namespace std;
 
+long long int max(long long int a, long long int b){
+	if (a > b){
+		return a;
+	}
+	return b;
+}
+long long int min(long long int a, long long int b){
+	if (a < b){
+		return a;
+	}
+	return b;
+}
+
 int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int tc;
+	long long int temp;
+	
+	cin >> tc;
 
-    int tc, n;
-    cin >> tc;
+	while (tc--){
+		long long int n, mx, mn, diff = LONG_LONG_MIN;
+		cin >> n;
+		long long int arr[n];
 
-    while (tc--){
-        
-        cin >> n;
+		for (int i = 0; i < n; i++){
+			cin >> arr[i];
+		}
+		mx = mn = arr[0];
+		for (int i = 1; i < n; i++){
+			diff = max(diff, max(mx-arr[i], mx-arr[i]));
+			mx = max(mx, arr[i]);
+			mn = min(mn, arr[i]);
+		}
 
-        for (int i = 0; i < n; i++){
+		cout << diff << "\n";
+	}
 
-        }
-    }
-}     
+	return 0;
+}
