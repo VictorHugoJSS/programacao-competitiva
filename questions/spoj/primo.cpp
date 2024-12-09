a9df7f20ea5376afe1325de2fcc3cf7b6ce37a3e
Victor Hugo José Sales da Silva <victor.-hugo@outlook.com.br>
2024-12-09 17:48:49 -0300

    Create primo.cpp

---
+++ questions/spoj/primo.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool is_prime(long long n){
+    
+    if (n == 1 || n == 0){
+        return false;
+    }
+    
+    for (int i = 2; i < n+1; i++){
+        if (n%i == 0 && i != n){
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+int main(){
+    long long n;
+    
+    scanf("%lld", &n);
+    
+    if (is_prime(n)){
+        printf("sim\n");
+    }
+    else{
+        printf("nao\n");
+    }
+    
+    return 0;
+}
