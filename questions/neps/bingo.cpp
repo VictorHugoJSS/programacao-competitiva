4bbe952a6d6439d82294f0e0973a015f52ca0a1b
VictorHugoJSS <vh9325862@gmail.com>
2025-05-03 19:57:38 -0300

    bingo feito

--- questions/neps/bingo.cpp
+++ questions/neps/bingo.cpp
@@ -8,12 +8,31 @@ int main(){
     int n, b, temp;
     scanf("%d %d", &n, &b);
 
-    while (n && b){
+    while (n || b){
+        vector<int> arr(b);
+        set<int> nums;
 
         for (int i = 0; i < b; i++){
-            cin >> temp;
-            vi.push_back(temp);
+            scanf("%d", &arr[i]);
         }
+        if (b == n+1){
+            printf("Y\n");
+        }
+        else{
+            for (int i = 0; i < arr.size()-1; i++){
+                for (int j = 0; j < arr.size(); j++){
+                    nums.insert(abs(arr[i]-arr[j]));
+                }
+            }
+
+            if (nums.size() == n+1){
+                printf("Y\n");
+            }
+            else{
+                printf("N\n");
+            }
+        }
+
         scanf("%d %d", &n, &b);
     }
     return 0;
